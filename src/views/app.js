32f1c466408af9220bb1ce64b0574e14c8614de1
Hack Reactor Student <student@hackreactor.com>
2017-05-31 23:32:14 -0700

    adds video player functionality up to but not including clicks

--- src/views/app.js
+++ src/views/app.js
@@ -5,14 +5,24 @@ var AppView = Backbone.View.extend({
   initialize: function() {
     this.videos = new Videos();
     this.render();
+    // this.set({renderedVideo: 0});
+    console.log(this);
+    console.log(this.get('renderedVideo'));
+    
   },
 
+  renderedVideo: function(videoID) {
+    return window.exampleVideoData[videoID];
+  },
+  
+  videoID: 0,
 
   render: function() {
+    // console.log('renderedVideo', this.renderedVideo);
+    // this.videoID = 0;
     this.$el.html(this.template());
-    var exampleData = new Videos(window.exampleVideoData);
-     console.log('exampleData');
     new VideoListView({el: '.list', collection: window.exampleVideoData});
+    var videoPlayerView = new VideoPlayerView({model: this.renderedVideo(this.videoID)});
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -10,10 +10,19 @@ var VideoListView = Backbone.View.extend({
     this.$el.children().detach();
     this.$el.html(this.template());
     var videoList = this.collection.map(function(child) {
-      console.log('child: ', child);
-      return new VideoListEntryView({model: child}).render().el;
+      return new VideoListEntryView({model: child}).el;
     });
-    $('.video-list').append(videoList);
+    _.each(videoList, function(video, i) {
+      video.setAttribute('videoID', i);
+      $('.video-list').append(video);
+
+      $('[videoID=' + i + ']').on('click', function(event) {
+        console.log(Backbone.View.videoID);
+        console.log(video.getAttribute('videoid'));
+      });
+      
+    });
+    // $('.video-list').append(videoList);
     return this;
   },
 
